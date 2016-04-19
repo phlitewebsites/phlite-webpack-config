e93710f03c43c91f8823d2bc7a71f8cd88ecbaf6
Dan Hayden <pythonic1@gmail.com>
2016-04-19 16:29:04 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var glob = require('glob');
+var webpack = require('webpack');
+
+var rootPath = path.resolve(__dirname, '..');
+
+var uglifyOptions = {
+  minimize: true,
+  output: {comments: false},
+  compressor: {warnings: false}
+};
+
+var babelOptions = {
+  test: /.js$/,
+  loader: 'babel-loader',
+  exclude: /node_modules/,
+  query: {
+    compact: true,
+    presets: ['es2015']
+  }
+};
+
+module.exports = {
+  entry: getEntries(),
+  output: {
+    path: rootPath + '/public/js',
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({'process.env': {NODE_ENV: JSON.stringify('production')}}),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(uglifyOptions)
+  ],
+  module: {
+    loaders: [babelOptions]
+  }
+};
+
+function getEntries () {
+  var files = glob.sync('./src/scripts/{*,bundles/*}.js');
+
+  return files.reduce(function (entries, file) {
+    var name = path.basename(file, '.js');
+    entries[name] = file;
+    return entries;
+  }, {});
+}
